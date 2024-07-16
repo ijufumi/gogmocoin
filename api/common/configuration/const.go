61d329e42e09a11e9f6c5705d1f99b60b5f7490c
Takafumi Iju <ijufumi@gmail.com>
2024-07-16 10:24:15 +0900

    feat: Add IntervalType const

--- api/common/configuration/const.go
+++ api/common/configuration/const.go
@@ -194,6 +194,24 @@ const (
 	CancelTypePositionLossCut = CancelType("POSITION_LOSSCUT")
 )
 
+// IntervalType ...
+type IntervalType string
+
+const (
+	IntervalType1Min   = IntervalType("1min")
+	IntervalType5Min   = IntervalType("5min")
+	IntervalType10Min  = IntervalType("10min")
+	IntervalType15Min  = IntervalType("15min")
+	IntervalType30Min  = IntervalType("30min")
+	IntervalType1Hour  = IntervalType("1hour")
+	IntervalType4Hour  = IntervalType("4hour")
+	IntervalType8Hour  = IntervalType("8hour")
+	IntervalType12Hour = IntervalType("12hour")
+	IntervalType1Day   = IntervalType("1day")
+	IntervalType1Week  = IntervalType("1week")
+	IntervalType1Month = IntervalType("1month")
+)
+
 const (
 	PublicRestAPIHost  = "https://api.coin.z.com/public"
 	PrivateRestAPIHost = "https://api.coin.z.com/private"
