866ded963efdc52336844abd414fdae01e5cae7c
Takafumi Iju <ijufumi@gmail.com>
2024-07-17 07:02:50 +0900

    feat: implementation of public KLines API

---
+++ api/public/rest/klines.go
@@ -0,0 +1,49 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/ijufumi/gogmocoin/api/common/api"
+	"github.com/ijufumi/gogmocoin/api/common/configuration"
+	"github.com/ijufumi/gogmocoin/api/public/rest/model"
+	"net/url"
+)
+
+type KLines interface {
+	KLines(symbol configuration.Symbol, intervalType configuration.IntervalType, date string) (*model.KLinesRes, error)
+}
+
+func NewKLines() klines {
+	return klines{
+		RestAPIBase: api.NewRestAPIBase(),
+	}
+}
+
+type klines struct {
+	api.RestAPIBase
+}
+
+func (k *klines) KLines(symbol configuration.Symbol, intervalType configuration.IntervalType, date string) (*model.KLinesRes, error) {
+	param := url.Values{
+		"symbol":   {string(symbol)},
+		"interval": {string(intervalType)},
+		"date":     {date},
+	}
+
+	res, err := o.Get(param, "/v1/klines")
+	if err != nil {
+		return nil, err
+	}
+
+	kLinesRes := new(model.KLinesRes)
+	err = json.Unmarshal(res, kLinesRes)
+	if err != nil {
+		return nil, fmt.Errorf("[KLines]error:%v,body:%s", err, res)
+	}
+
+	if kLinesRes.Status != 0 {
+		return nil, fmt.Errorf("%v", kLinesRes.Messages)
+	}
+
+	return kLinesRes, nil
+}
