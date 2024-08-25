50b8dfb0e70237097aeea1b4054cb1a5f9442199
Takafumi Iju <ijufumi@gmail.com>
2024-08-25 09:17:16 +0900

    feat: add private websocket base

---
+++ api/private/ws/internal/ws_api_base.go
@@ -0,0 +1,27 @@
+package internal
+
+import "github.com/ijufumi/gogmocoin/v2/api/internal/api"
+
+type PrivateWSAPIBase struct {
+	wsAPIBase   *api.WSAPIBase
+	restAPIBase api.RestAPIBase
+}
+
+func NewPrivateWSAPIBase(apiKey, secretKey string, requestFactory api.RequestFactoryFunc) *PrivateWSAPIBase {
+	return &PrivateWSAPIBase{
+		restAPIBase: api.NewPrivateRestAPIBase(apiKey, secretKey),
+		wsAPIBase:   api.NewWSAPIBase(requestFactory),
+	}
+}
+
+func (w *PrivateWSAPIBase) SetGetHostFunc(f api.GetHostFunc) {
+	w.wsAPIBase.SetGetHostFunc(f)
+}
+
+func (w *PrivateWSAPIBase) Subscribe() error {
+	return w.wsAPIBase.Subscribe()
+}
+
+func (w *PrivateWSAPIBase) Unsubscribe() error {
+	return w.wsAPIBase.Unsubscribe()
+}
