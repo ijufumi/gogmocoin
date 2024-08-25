b9cc64cb0919bb6e6903937be7bb88fd69af82fc
Takafumi Iju <ijufumi@gmail.com>
2024-08-26 06:48:10 +0900

    feat: set private  websocket api host factory

--- api/private/ws/internal/ws_api_base.go
+++ api/private/ws/internal/ws_api_base.go
@@ -1,17 +1,31 @@
 package internal
 
-import "github.com/ijufumi/gogmocoin/v2/api/internal/api"
+import (
+	"fmt"
+	"github.com/ijufumi/gogmocoin/v2/api/common/consts"
+	"github.com/ijufumi/gogmocoin/v2/api/internal/api"
+	"sync/atomic"
+	"time"
+)
 
 type PrivateWSAPIBase struct {
 	wsAPIBase   *api.WSAPIBase
 	restAPIBase api.RestAPIBase
+	token       *atomic.Value
+}
+
+type token struct {
+	Token      string
+	ExpireTime time.Time
 }
 
 func NewPrivateWSAPIBase(apiKey, secretKey string, requestFactory api.RequestFactoryFunc) *PrivateWSAPIBase {
-	return &PrivateWSAPIBase{
+	wsAPIBase := &PrivateWSAPIBase{
 		restAPIBase: api.NewPrivateRestAPIBase(apiKey, secretKey),
 		wsAPIBase:   api.NewWSAPIBase(requestFactory),
 	}
+	wsAPIBase.SetHostFactoryFunc(wsAPIBase.hostFactory)
+	return wsAPIBase
 }
 
 func (w *PrivateWSAPIBase) SetHostFactoryFunc(f api.HostFactoryFunc) {
@@ -24,4 +38,16 @@ func (w *PrivateWSAPIBase) Subscribe() error {
 
 func (w *PrivateWSAPIBase) Unsubscribe() error {
 	return w.wsAPIBase.Unsubscribe()
+}
+
+func (w *PrivateWSAPIBase) hostFactory() string {
+	rawValue := w.token.Load()
+	if rawValue == nil {
+		return consts.PrivateWSAPIHost
+	}
+	tokenValue, ok := rawValue.(token)
+	if !ok {
+		return consts.PrivateWSAPIHost
+	}
+	return fmt.Sprintf("%s/%s", consts.PrivateWSAPIHost, tokenValue.Token)
 }
